02153fd5493378d94361294dfcde622e9b234ce1
mattbolanos <matthew.a.bolanos@gmail.com>
2024-12-03 22:03:19 -0500

    favicon stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,17 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "San Marino Towers Challenge",
   description: "San Marino Towers Challenge - Made for Jet Lag: The Game",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
